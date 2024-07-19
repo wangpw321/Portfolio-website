130077aa42a341ea4aaa695a56b25c7f1b32c7fc
wangpalvin@gmail.com <wangpalvin@gmail.com>
2024-07-19 09:52:26 -0500

    Update summary

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,16 @@ export default function About() {
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-            Hi, I'm Puwei.
+            Hi, I'm Palvin.
             <br className="hidden lg:inline-block" />I love to build amazing
             apps.
           </h1>
           <p className="mb-4 leading-relaxed">
-            I have graduated the University of Winnipeg in December 2021 with aquiring master of science in applied computer science. I obsess over the minor details in software development and am passionate in getting computers to more effectively work on your side by freeing you of the burdens of carrying out repetitive tasks through computer science.
-            I am interested in full-stack development, DevOps and C/C++ development.
-          </p>
-          <p className="mb-8 leading-relaxed text-purple-400 text-lg font-serif font-bold">
-            Research Area: Image Processing, Computer Vison and Parallel Computing
+          I am a software developer based in Winnipeg, Canada, with extensive experience in the software development industry and a Master’s degree in Computer Science. I am dedicated to perfecting the finer details of software development and passionate about leveraging technology to streamline processes and automate repetitive tasks.
+
+Explore my tech stack below, and feel free to reach out to discuss how I can contribute to your business.
+
+
           </p>
           <div className="mb-8 flex justify-center">
             <a
